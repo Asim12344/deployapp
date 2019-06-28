ace85f324acb821df36b3a4ace30ad3635250741
Asim12344 <m.asimwaqar18@gmail.com>
2019-06-28 17:41:04 +0500

    Updates

---
+++ precache-manifest.1294eb9eb7fe50eccd250f8d30ceeaaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd508ceb388fbc8d758905a7ff17487",
+    "url": "/deployapp/index.html"
+  },
+  {
+    "revision": "1eec881732173455c0a9",
+    "url": "/deployapp/static/css/main.f8cf45af.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/deployapp/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "1eec881732173455c0a9",
+    "url": "/deployapp/static/js/main.7aa4daf9.chunk.js"
+  },
+  {
+    "revision": "4e0dd4522585433b8d09",
+    "url": "/deployapp/static/js/runtime~main.996fb30d.js"
+  }
+]);
